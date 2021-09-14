d24f108466f0ae4ef77de78e5c9264e57b7fc1f1
Anton Sadyrin <aysad26@yandex.ru>
2021-09-14 14:47:21 +0300

    56

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,8 @@
-let popup = document.querySelector('.popup');
-let buttonTypeEdit = document.querySelector('.button_type_edit');
-let buttonTypeClose = document.querySelector('.button_type_close');
-let formElement = document.querySelector('.form');
-let nameInput = document.querySelector('.form__item_type_name');
-let jobInput = document.querySelector('.form__item_type_job');
-let name = document.querySelector('.profile__title');
-let job = document.querySelector('.profile__subtitle');
-
-
-function openPopup() {
-    popup.classList.add('popup_opened')
-    nameInput.value = name.textContent
-    jobInput.value = job.textContent
-};
-
-function closePopup() {
-    popup.classList.remove('popup_opened')
-};
-
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    name.textContent = nameInput.value;
-    job.textContent = jobInput.value;
-    closePopup();
-}
-
-formElement.addEventListener('submit', formSubmitHandler);
-buttonTypeEdit.addEventListener('click', openPopup);
-buttonTypeClose.addEventListener('click', closePopup);
-
+  const button = document.querySelector('.menu__burger-button');
+  const navigation = document.querySelector('.navigation')
+     
+  button.addEventListener('click', (e) => {
+    button.classList.toggle('menu__burger-button_active');
+    navigation.classList.toggle('navigation__active')
+  });
 
